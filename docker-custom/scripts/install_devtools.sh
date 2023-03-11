bc3d9b3ba8db5ef6f9bd3457e795b05e9b12c488
竹子 <69517628+being233@users.noreply.github.com>
2023-03-11 22:44:10 +0800

    Update install_devtools.sh

--- docker-custom/scripts/install_devtools.sh
+++ docker-custom/scripts/install_devtools.sh
@@ -4,7 +4,7 @@ set -ex
 # Installing Devtools
 if [[ ${TAG_SUFFIX} != *"minimal" ]]; then
   echo "Installing devtools"
-  apk add --no-cache --virtual devtools build-base linux-headers udev python3
+  apk add --no-cache --virtual devtools build-base linux-headers udev python3 python3-dev
 else
   echo "Skip installing devtools"
 fi
